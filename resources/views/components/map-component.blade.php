edc8e2ef6c0daf52bf7f57e39df34278ddb606de
Dodo <sheraldohalim.sh@gmail.com>
2024-10-05 20:53:50 +0700

    ganti map pake mapbox

--- resources/views/components/map-component.blade.php
+++ resources/views/components/map-component.blade.php
@@ -1,134 +1,165 @@
-<div id="map" style="height: 500px; width: 100%; position: relative; z-index: 1 ;"></div>
+<div id="map" style="height: 500px; width: 100%; position: relative; z-index: 1;"></div>
 
 @push('styles')
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
     <style>
         .mobile-menu {
-            position: relative; 
+            position: relative;
             z-index: 10;
         }
     </style>
 @endpush
 
 @push('scripts')
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             // Define the flood locations and city details
             const floodLocations = [
-                { latlng: [-6.138414, 106.863956], city: "Jakarta Utara", count: 8 },
-                { latlng: [-6.1615, 106.7570], city: "Jakarta Barat", count: 15 },
-                { latlng: [-6.225014, 106.900447], city: "Jakarta Timur", count: 3 },
-                { latlng: [-6.261493, 106.810600], city: "Jakarta Selatan", count: 4 },
+                { lnglat: [106.863956, -6.138414], city: "Jakarta Utara", count: 8 },
+                { lnglat: [106.7570, -6.1615], city: "Jakarta Barat", count: 15 },
+                { lnglat: [106.900447, -6.225014], city: "Jakarta Timur", count: 3 },
+                { lnglat: [106.810600, -6.261493], city: "Jakarta Selatan", count: 4 },
             ];
 
             const cityDetails = {
                 "Jakarta Barat": [
-                    { latlng: [-6.140227, 106.7235353], kelurahan: "Cengkareng Barat", indeksBanjir: 1.8, Kategori: "Sedang"},
-                    { latlng: [-6.1416616, 106.7328601], kelurahan: "Cengkareng Timur", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.16888, 106.71605], kelurahan: "Duri Kosambi", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.13983, 106.75331], kelurahan: "Kapuk", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.1511336, 106.7575653], kelurahan: "Kedaung Kali Angke", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.16122, 106.73901], kelurahan: "Rawa Buaya", indeksBanjir: 2.2, Kategori: "Tinggi" },
-                    { latlng: [-6.1489448, 106.7823051], kelurahan: "Jelambar Baru", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.17116, 106.78476], kelurahan: "Tanjung Duren Utara", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.16684, 106.70250], kelurahan: "Semanan", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.1198254, 106.718693], kelurahan: "Tegal Alur", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.18190, 106.75909], kelurahan: "Kedoya Selatan", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.16781, 106.76152], kelurahan: "Kedoya Utara", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.21895, 106.73918], kelurahan: "Joglo", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.18999, 106.73647], kelurahan: "Kembangan Selatan", indeksBanjir: 2.0, Kategori: "Tinggi"},
-                    { latlng: [-6.17179, 106.74394], kelurahan: "Kembangan Utara", indeksBanjir: 2.0, Kategori: "Tinggi" }
+                    { lnglat: [106.7235353, -6.140227], kelurahan: "Cengkareng Barat", indeksBanjir: 1.8, Kategori: "Sedang"},
+                    { lnglat: [106.7328601, -6.1416616], kelurahan: "Cengkareng Timur", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.71605, -6.16888], kelurahan: "Duri Kosambi", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.75331, -6.13983], kelurahan: "Kapuk", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.7575653, -6.1511336], kelurahan: "Kedaung Kali Angke", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.73901, -6.16122], kelurahan: "Rawa Buaya", indeksBanjir: 2.2, Kategori: "Tinggi" },
+                    { lnglat: [106.7823051, -6.1489448], kelurahan: "Jelambar Baru", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.78476, -6.17116], kelurahan: "Tanjung Duren Utara", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.70250, -6.16684], kelurahan: "Semanan", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.718693, -6.1198254], kelurahan: "Tegal Alur", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.75909, -6.18190], kelurahan: "Kedoya Selatan", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.76152, -6.16781], kelurahan: "Kedoya Utara", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.73918, -6.21895], kelurahan: "Joglo", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.73647, -6.18999], kelurahan: "Kembangan Selatan", indeksBanjir: 2.0, Kategori: "Tinggi"},
+                    { lnglat: [106.74394, -6.17179], kelurahan: "Kembangan Utara", indeksBanjir: 2.0, Kategori: "Tinggi" }
                 ],
                 "Jakarta Utara": [
-                    { latlng: [-6.12292, 106.92662], kelurahan: "Semper Barat", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.12093, 106.93517], kelurahan: "Semper Timur", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.15233, 106.92796], kelurahan: "Sukapura", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.1626344, 106.9158465], kelurahan: "Pegangsaan Dua", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.1140999, 106.9116460], kelurahan: "Lagoa", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.1224924, 106.9121406], kelurahan: "Tugu Utara", indeksBanjir: 1.6, Kategori: "Sedang" },
-                    { latlng: [-6.13550, 106.78545], kelurahan: "Pejagalan", indeksBanjir: 2.2, Kategori: "Tinggi" },
-                    { latlng: [-6.125643, 106.800759], kelurahan: "Penjaringan", indeksBanjir: 1.8, Kategori: "Sedang" }
+                    { lnglat: [106.92662, -6.12292], kelurahan: "Semper Barat", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.93517, -6.12093], kelurahan: "Semper Timur", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.92796, -6.15233], kelurahan: "Sukapura", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.9158465, -6.1626344], kelurahan: "Pegangsaan Dua", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.9116460, -6.1140999], kelurahan: "Lagoa", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.9121406, -6.1224924], kelurahan: "Tugu Utara", indeksBanjir: 1.6, Kategori: "Sedang" },
+                    { lnglat: [106.78545, -6.13550], kelurahan: "Pejagalan", indeksBanjir: 2.2, Kategori: "Tinggi" },
+                    { lnglat: [106.800759, -6.125643], kelurahan: "Penjaringan", indeksBanjir: 1.8, Kategori: "Sedang" }
                 ],
                 "Jakarta Timur": [
-                    { latlng: [-6.23471, 106.86651], kelurahan: "Bidara Cina", indeksBanjir: 2.4, Kategori: "Tinggi" },
-                    { latlng: [-6.21850, 106.86132], kelurahan: "Kampung Melayu", indeksBanjir: 2.2, Kategori: "Tinggi" },
-                    { latlng: [-6.19978, 106.90110], kelurahan: "Jatinegara Kaum", indeksBanjir: 1.6, Kategori: "Tinggi" }
+                    { lnglat: [106.86651, -6.23471], kelurahan: "Bidara Cina", indeksBanjir: 2.4, Kategori: "Tinggi" },
+                    { lnglat: [106.86132, -6.21850], kelurahan: "Kampung Melayu", indeksBanjir: 2.2, Kategori: "Tinggi" },
+                    { lnglat: [106.90110, -6.19978], kelurahan: "Jatinegara Kaum", indeksBanjir: 1.6, Kategori: "Tinggi" }
                 ],
                 "Jakarta Selatan": [
-                    { latlng: [-6.23404, 106.82440], kelurahan: "Kuningan Barat", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.25085, 106.82211], kelurahan: "Mampang Prapatan", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.24694, 106.81625], kelurahan: "Pela Mampang", indeksBanjir: 1.8, Kategori: "Sedang" },
-                    { latlng: [-6.25884, 106.85500], kelurahan: "Rawajati", indeksBanjir: 2.0, Kategori: "Tinggi" }
+                    { lnglat: [106.82440, -6.23404], kelurahan: "Kuningan Barat", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.82211, -6.25085], kelurahan: "Mampang Prapatan", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.81625, -6.24694], kelurahan: "Pela Mampang", indeksBanjir: 1.8, Kategori: "Sedang" },
+                    { lnglat: [106.85500, -6.25884], kelurahan: "Rawajati", indeksBanjir: 2.0, Kategori: "Tinggi" }
                 ],
             };
 
-            // Initialize the map
-            const map = L.map("map").setView([-6.2088, 106.8456], 11);
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZG9kb3hkIiwiYSI6ImNtMXNzc3o2eDBham0ya3BybjAzdHh6dTUifQ.j4wY9CcmmjYGbPizv6b-Dg';
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [106.8456, -6.2088], // Jakarta coordinates
+                zoom: 10,
+                pitch: 45, // Add 3D perspective
+                bearing: -17.6,
+                projection: 'globe'
+            });
 
-            // Add the OpenStreetMap tile layer
-            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                attribution: "© OpenStreetMap contributors",
-            }).addTo(map);
+            map.on('load', () => {
+                // Add 3D building layer
+                map.addLayer({
+                    'id': '3d-buildings',
+                    'source': 'composite',
+                    'source-layer': 'building',
+                    'filter': ['==', 'extrude', 'true'],
+                    'type': 'fill-extrusion',
+                    'minzoom': 15,
+                    'paint': {
+                        'fill-extrusion-color': '#aaa',
+                        'fill-extrusion-height': [
+                            "interpolate", ["linear"], ["zoom"],
+                            15, 0,
+                            15.05, ["get", "height"]
+                        ],
+                        'fill-extrusion-opacity': .6
+                    }
+                });
+
+                // Add markers for flood locations
+                floodLocations.forEach((location) => {
+                    const el = document.createElement('div');
+                    el.className = 'marker';
+                    el.style.backgroundColor = '#3388ff';
+                    el.style.width = '18px';
+                    el.style.height = '18px';
+                    el.style.borderRadius = '50%';
+                    el.style.border = '2px solid #000';
+
+                    const marker = new mapboxgl.Marker(el)
+                        .setLngLat(location.lnglat)
+                        .setPopup(new mapboxgl.Popup({ offset: 25 })
+                            .setHTML(`<h3>${location.city}</h3><p>Total Lokasi Banjir: ${location.count}</p>`))
+                        .addTo(map);
+
+                    marker.getElement().addEventListener('click', () => {
+                        console.log(`Marker clicked: ${location.city}`);
+                        toggleCityDetails(location.city);
+                    });
+                });
+            });
 
             const detailMarkers = [];
             let currentCity = null;
 
-            // Add flood locations markers
-            floodLocations.forEach((location) => {
-                const marker = L.circleMarker(location.latlng, {
-                    radius: 9,
-                    fillColor: "#3388ff",
-                    color: "#000",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8,
-                }).addTo(map);
-
-                marker.on("click", () => {
-                    console.log(`Marker clicked: ${location.city}`);
-                    toggleCityDetails(location.city);
-                });
-
-                marker.bindPopup(`Kota: ${location.city}<br>Total Lokasi Banjir: ${location.count}`);
-            });
-
-            // Function to show details for a specific city
             window.showDetails = function(city) {
                 console.log(`Showing details for: ${city}`);
-                const cityCenter = floodLocations.find((loc) => loc.city === city)?.latlng;
+                const cityCenter = floodLocations.find((loc) => loc.city === city)?.lnglat;
                 if (cityCenter) {
-                    map.setView(cityCenter, 11);
+                    map.flyTo({
+                        center: cityCenter,
+                        zoom: 10,
+                        essential: true
+                    });
                 }
 
-                const markers = cityDetails[city].map((location) => {
-                    const detailMarker = L.circleMarker(location.latlng, {
-                        radius: 7,
-                        fillColor: location.indeksBanjir >= 2.0 ? "#FF0000" : "#FFA500",
-                        color: "#000",
-                        weight: 1,
-                        opacity: 1,
-                        fillOpacity: 0.8,
-                    }).addTo(map);
-
-                    detailMarker.bindPopup(`Kelurahan: ${location.kelurahan}<br>Indeks Banjir: ${location.indeksBanjir}<br>Kategori: ${location.Kategori}`);
-                    return detailMarker;
-                });
-
                 // Clear previous markers
-                detailMarkers.forEach((m) => map.removeLayer(m));
+                detailMarkers.forEach((marker) => marker.remove());
                 detailMarkers.length = 0;
 
-                // Store new markers
-                detailMarkers.push(...markers);
+                cityDetails[city].forEach((location) => {
+                    const el = document.createElement('div');
+                    el.className = 'detail-marker';
+                    el.style.backgroundColor = location.indeksBanjir >= 2.0 ? "#FF0000" : "#FFA500";
+                    el.style.width = '14px';
+                    el.style.height = '14px';
+                    el.style.borderRadius = '50%';
+                    el.style.border = '1px solid #000';
+
+                    const marker = new mapboxgl.Marker(el)
+                        .setLngLat(location.lnglat)
+                        .setPopup(new mapboxgl.Popup({ offset: 25 })
+                            .setHTML(`<h3>${location.kelurahan}</h3><p>Indeks Banjir: ${location.indeksBanjir}<br>Kategori: ${location.Kategori}</p>`))
+                        .addTo(map);
+
+                    detailMarkers.push(marker);
+                });
+
                 currentCity = city;
             };
 
-            // Function to toggle city details
             function toggleCityDetails(city) {
                 if (currentCity === city) {
                     // Hide details if the same city is clicked
-                    detailMarkers.forEach((m) => map.removeLayer(m));
+                    detailMarkers.forEach((marker) => marker.remove());
                     detailMarkers.length = 0;
                     currentCity = null;
                 } else {
@@ -137,4 +168,4 @@
             }
         });
     </script>
-@endpush
+@endpush  
